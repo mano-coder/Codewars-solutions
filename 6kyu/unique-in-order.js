afe05696811da2236bbb392e0fa57c4db684e90d
mano-coder <bis.anouar5@gmail.com>
2026-04-24 14:27:24 +0100

    add: unique-in-order solution

--- 6kyu/unique-in-order.js
+++ 6kyu/unique-in-order.js
@@ -4,4 +4,31 @@
 
 // Solution:
 
+function uniqueInOrder(str) {
+  let arr = []
+  if (str.length < 0) {
+    console.log(arr)
+    arr = str.split("");
+  } else {
+    arr = str;
+  }
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== arr[i + 1]) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+}
 
+console.log(uniqueInOrder("AAAABBBCCDAABBB"));
+console.log(uniqueInOrder("ABBCcAD"));
+console.log(uniqueInOrder([1, 2, 2, 3, 3]));
+
+
+// Pro solution:
+/*
+  var uniqueInOrder=function(iterable){
+    return [...iterable].filter((a, i) => a !== iterable[i-1])
+}
+*/
