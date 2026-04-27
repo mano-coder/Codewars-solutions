7d59238378bbad7db911c64ba9cf76cf758d6d3a
mano-coder <bis.anouar5@gmail.com>
2026-04-28 00:26:12 +0100

    add solution

--- 6kyu/count-chars-in-string.js
+++ 6kyu/count-chars-in-string.js
@@ -1,4 +1,11 @@
 function count(string) {
+  const result = {};
+  for (const char of string) {
+    if (char === " ") continue;
+    result[char] = (result[char] || 0) + 1;
+  };
+  return result;
 }
+
 console.log(count("aba fkjlsdfeven twhen thing go wrong jflasj"));
 console.log(count("      "));
