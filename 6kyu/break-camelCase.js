8accb09a6c8a92b882c0e0f4c82cb56c18a1ec34
mano-coder <bis.anouar5@gmail.com>
2026-04-27 23:18:27 +0100

    chore: add challenge description

--- 6kyu/break-camelCase.js
+++ 6kyu/break-camelCase.js
@@ -1,3 +1,9 @@
+/*
+Complete the solution so that the function will break up camel casing, 
+  using a space between words.
+ */
+
+// my solution
 function solution(string) {
   return [...string]
     .map((char) => (char === char.toLowerCase() ? char : ` ${char}`))
