174eb54cb3ce5746b6e3ec069b9ddbca95094035
mano-coder <bis.anouar5@gmail.com>
2026-04-28 21:27:00 +0100

    first try

--- 6kyu/build-tower-advanced.js
+++ 6kyu/build-tower-advanced.js
@@ -1,3 +1,14 @@
 function towerBuilder(nFloors, nBlockSz) {
+  // build here
+  const blockBuilder = (width, height) => {
+    const block = [];
+    for (let i = 0; i <= height; i++) {
+      block.push("*".repeat(width));
+    }
+    return block;
+  };
+  const [w, h] = nBlockSz;
+  return blockBuilder(w, h);
 }
+
 console.log(towerBuilder(1, [1, 2]));
