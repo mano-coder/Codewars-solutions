33a7ed52ca7aa7b6bc8a15a41368b5d7523b1d77
mano-coder <bis.anouar5@gmail.com>
2026-04-28 20:24:34 +0100

    directions-reduction.js solution

--- 6kyu/directions-reduction.js
+++ 6kyu/directions-reduction.js
@@ -1,4 +1,21 @@
 function dirReduc(arr) {
+  const opposites = {
+    NORTH: "SOUTH",
+    SOUTH: "NORTH",
+    WEST: "EAST",
+    EAST: "WEST",
+  };
+
+  return arr.reduce((stack, dir) => {
+    const lastDir = stack[stack.length - 1];
+
+    if (lastDir === opposites[dir]) {
+      stack.pop();
+    } else {
+      stack.push(dir);
+    }
+    return stack;
+  }, []);
 }
 
 console.log(
