43b03608ad257c8419c17304d6a3327d0503984f
mano-coder <bis.anouar5@gmail.com>
2026-04-28 20:26:15 +0100

    chore: add description

--- 6kyu/directions-reduction.js
+++ 6kyu/directions-reduction.js
@@ -1,3 +1,10 @@
+/*
+Write a function dirReduc which will take an array of strings and returns an 
+array of strings with the needless directions removed 
+(W<->E or S<->N side by side).
+*/
+
+// my solution
 function dirReduc(arr) {
   const opposites = {
     NORTH: "SOUTH",
