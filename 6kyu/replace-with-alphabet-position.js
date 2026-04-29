f30f25d6ff6587f88c3718b044f5ff2391602329
mano-coder <bis.anouar5@gmail.com>
2026-04-29 19:21:36 +0100

    pro solution

--- 6kyu/replace-with-alphabet-position.js
+++ 6kyu/replace-with-alphabet-position.js
@@ -9,3 +9,19 @@ function alphabetPosition(text) {
   });
   return result.join(" ");
 }
+
+// pro solution
+/*
+function alphabetPosition(text) {
+  var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+  return text
+    .toLowerCase()
+    .split("")
+    .filter((letter) => {
+      let index = alphabet.indexOf(letter);
+      return index > -1;
+    })
+    .map((letter) => alphabet.indexOf(letter) + 1)
+    .join(" ");
+}
+*/
