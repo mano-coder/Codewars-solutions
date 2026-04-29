bd36aed9bb4acb461dd1e6b356042a9e4258251c
mano-coder <bis.anouar5@gmail.com>
2026-04-29 21:00:10 +0100

    chore: add challenge description

--- 6kyu/persistent-bugger.js
+++ 6kyu/persistent-bugger.js
@@ -1,3 +1,10 @@
+/*
+Write a function, persistence, that takes in a positive parameter num and 
+returns its multiplicative persistence, which is the number of times you must 
+multiply the digits in num until you reach a single digit.
+*/
+
+// my solution
 function persistence(num) {
   if (num <= 9) return 0;
   let count = 0;
@@ -13,3 +20,11 @@ function persistence(num) {
 }
 
 console.log(persistence(4));
+
+// pro solution
+function persistence(num) {
+   for (var i = 0; num > 9; i++) {
+     num = num.toString().split('').reduce((t, c) => c * t);
+   }
+   return i;
+}
