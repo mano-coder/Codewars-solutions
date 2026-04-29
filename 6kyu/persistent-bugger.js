ea13e5314ba92bcccfd7d03c446465a1a39e4157
mano-coder <bis.anouar5@gmail.com>
2026-04-29 20:58:03 +0100

    my solution

--- 6kyu/persistent-bugger.js
+++ 6kyu/persistent-bugger.js
@@ -1,3 +1,13 @@
 function persistence(num) {
-   //code me
+  if (num <= 9) return 0;
+  let count = 0;
+  while (num > 9) {
+    const digits = Array.from(String(num), Number);
+    num = digits.reduce((acc, dig) => {
+      num = dig * acc;
+      return num;
+    });
+    count++;
+  }
+  return count;
 }
