7e7e949ab83391aca19f298e92d26b5a493ca039
mano-coder <bis.anouar5@gmail.com>
2026-04-29 19:21:19 +0100

    my solution

--- 6kyu/replace-with-alphabet-position.js
+++ 6kyu/replace-with-alphabet-position.js
@@ -1,3 +1,11 @@
 function alphabetPosition(text) {
-  return text;
+  const alpha = "abcdefghijklmnopqrstuvwxyz".split("");
+  const arr = [...text];
+  let result = [];
+  arr.map((char) => {
+    char = char.toLowerCase();
+    if (!alpha.includes(char)) return;
+    result.push(alpha.indexOf(char) + 1);
+  });
+  return result.join(" ");
 }
