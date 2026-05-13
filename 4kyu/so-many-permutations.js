be871f6018f8c6a1a466c3fd985d64c6969e5c31
mano-coder <bis.anouar5@gmail.com>
2026-05-13 23:05:22 +0100

    chore: add comments

--- 4kyu/so-many-permutations.js
+++ 4kyu/so-many-permutations.js
@@ -1,9 +1,20 @@
+/*
+In this kata, your task is to create all permutations of a non-empty input
+string and remove duplicates, if present.
+
+Create as many "shufflings" as you can!
+*/
+
+// my solution
 function permutations(string) {
   if (string.length === 1) return [string];
   let result = [];
   for (let i = 0; i < string.length; i++) {
     const char = string[i];
-    let rest = string.split("").filter((_, idx) => idx !== i).join("");
+    let rest = string
+      .split("")
+      .filter((_, idx) => idx !== i)
+      .join("");
     const subpermutations = permutations(rest);
     for (let fo of subpermutations) {
       result.push([char, fo].join(""));
@@ -11,6 +22,25 @@ function permutations(string) {
   }
   return [...new Set(result)];
 }
+
+// clever way
+//function permutations(str) {
+//  return str.length <= 1
+//    ? [str]
+//    : Array.from(
+//        new Set(
+//          str
+//            .split("")
+//
+//            .map((char, i) =>
+//              permutations(str.substr(0, i) + str.substr(i + 1)).map(
+//                (p) => char + p,
+//              ),
+//            )
+//            .reduce((r, x) => r.concat(x), []),
+//        ),
+//      );
+//}
 
 console.log(permutations("abc"));
 console.log(permutations("aab"));
