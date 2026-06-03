b5bc58aceb57a3458d483ce4aceaceb205f61f9a
mano-coder <bis.anouar5@gmail.com>
2026-06-03 14:27:38 +0100

    add vigenere solution

--- 4kyu/vigenere-cipher-help.js
+++ 4kyu/vigenere-cipher-help.js
@@ -1,12 +1,49 @@
 class VigenèreCipher {
-    constructor(key, abc) {
-    }
+  constructor(key, abc) {
+    this.key = key;
+    this.abc = abc;
+  }
 
-    encode(str) {
-        return '';
+  encode(str) {
+    let result = "";
+    for (const [i, char] of [...str].entries()) {
+      if (this.abc.indexOf(char) === -1) {
+        result += char;
+        continue;
+      }
+      result +=
+        this.abc[
+          (this.abc.indexOf(char) +
+            this.abc.indexOf(this.key[i % this.key.length])) %
+            this.abc.length
+        ];
     }
+    return result;
+  }
 
-    decode(str) {
-        return '';
+  decode(str) {
+    let result = "";
+    for (const [i, char] of [...str].entries()) {
+      if (this.abc.indexOf(char) === -1) {
+        result += char;
+        continue;
+      }
+      result +=
+        this.abc[
+          (this.abc.indexOf(char) -
+            this.abc.indexOf(this.key[i % this.key.length]) +
+            this.abc.length) %
+            this.abc.length 
+        ];
     }
+    return result;
+  }
 }
+
+const foo = new VigenèreCipher("password", "abcdefghijklmnopqrstuvwxyz");
+
+const input = "hollywood";
+const encode = foo.encode(input);
+
+console.log(foo.encode(input));
+console.log(foo.decode(encode));
