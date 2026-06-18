a766fdd42e5460913ddb07aacd192a1cf4bd015a
mano <bis.anouar5@gmail.com>
2026-06-18 21:44:28 +0100

    complete solution

--- 4kyu/next-bigger-number.js
+++ 4kyu/next-bigger-number.js
@@ -1,8 +1,31 @@
 function nextBigger(n) {
-  console.log(typeof n)
-  const numArr = n.split("");
-  numArr[numArr.length - 1] = numArr[numArr.length - 2]
-  return numArr;
+  const arr = String(n).split("");
+  const len = arr.length
+
+  let i = len - 2;
+  while (i >= 0 && arr[i] > arr[i+1]) i--;
+
+  if (i < 0) return -1;
+
+  let j = len - 1;
+
+  while (arr[j] <= arr[i]) j--;
+
+  [arr[i], arr[j] = arr[j], arr[i]];
+
+  const suffix = arr.splice(i + 1).reverse()
+
+  arr.push(...suffix)
+
+  return Number(arr.join(""))
+
 }
 
-console.log(nextBigger(21))
+console.log(nextBigger(1234567908))
+console.log(nextBigger(1731))
+console.log(nextBigger(2017))
+console.log(nextBigger(144))
+console.log(nextBigger(2144))
+console.log(nextBigger(111))
+console.log(nextBigger(9))
+console.log(nextBigger(531))
